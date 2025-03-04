e4d6094d1f926f2be90c14cbec0084615e7a0656
Lê Tấn Bảo Bảo <110554959+baobao0303@users.noreply.github.com>
2025-03-04 23:46:06 +0700

    Feature: authentication controller (#3)
    * Initial project setup with server, configurations, and Prisma ORM
    
    * refactor(error-handling): improve error handling structure
    
    * feat: create a re-use middleware for validation and test postmain
    
    * setup: signup auth controller

---
+++ src/features/user/services/auth.service.ts
@@ -0,0 +1,3 @@
+class AuthService {}
+
+export const authService: AuthService = new AuthService()
--- src/globals/routes/appRoutes.ts
+++ src/globals/routes/appRoutes.ts
@@ -1,7 +1,9 @@
 import { Application } from 'express'
+import authRoute from '~/features/user/routes/auth.route'
 import userRoute from '~/features/user/routes/user.route'
 
 function appRoutes(app: Application) {
   app.use('/api/v1/users', userRoute)
+  app.use('/api/v1/auth', authRoute)
 }
 export default appRoutes
