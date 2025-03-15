edccf147a78858f13b5b89d43e58390f56316833
Lê Tấn Bảo Bảo <110554959+baobao0303@users.noreply.github.com>
2025-03-16 02:52:42 +0700

    feat(language-candidate): add relationship handling for language selection

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { Language, PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  const data: Language[] = [
+    {
+      name: 'english'
+    },
+    {
+      name: 'japanese'
+    },
+    {
+      name: 'chinese'
+    }
+  ]
+
+  await prisma.language.createMany({
+    data
+  })
+}
+
+main()
+  .then()
+  .catch((error) => console.log(error))
